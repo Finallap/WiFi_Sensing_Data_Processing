ad15546b2287995cd7c55eb5b32fcd64fc97330a
Finallap <547805712@qq.com>
2019-08-06 10:45:11 +0800

    单次迁移学习脚本

---
+++ LSTM/transfer_learning.m
@@ -0,0 +1,39 @@
+[x_train, y_train,  x_test, y_test] = split_train_test(csi_train, csi_label, 6, 0.7);%划分训练集和测试集
+
+numObservations = numel(x_train);
+for i=1:numObservations
+    sequence = x_train{i};
+    sequenceLengths(i) = size(sequence,2);
+end
+
+[sequenceLengths,idx] = sort(sequenceLengths);
+x_train = x_train(idx);
+y_train = y_train(idx);
+
+maxEpochs = 100;
+miniBatchSize = 32;
+
+options = trainingOptions('adam', ...
+        'ExecutionEnvironment','auto', ...
+        'GradientThreshold',1, ...
+        'MaxEpochs',maxEpochs, ...
+        'MiniBatchSize',miniBatchSize', ...
+        'SequenceLength','longest', ...
+        'Verbose',0, ...
+        'ValidationData',{x_test,y_test}, ...
+        'ValidationFrequency',5, ...
+        'InitialLearnRate',1e-4, ...
+        'Plots','training-progress');
+    
+net1 = trainNetwork(x_train,y_train,layers_1,options);
+
+y_Pred = classify(net1,x_test, ...
+    'SequenceLength','longest');
+
+acc = sum(y_Pred == y_test)./numel(y_test)
+ 
+figure('Units','normalized','Position',[0.2 0.2 0.4 0.4]);
+cm = confusionchart(y_test,y_Pred);
+cm.Title = 'Confusion Matrix for Validation Data';
+cm.ColumnSummary = 'column-normalized';
+cm.RowSummary = 'row-normalized';
