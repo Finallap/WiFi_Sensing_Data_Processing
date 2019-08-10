72bc8f2976a43677465844dd5d57453dd56dc409
Finallap <547805712@qq.com>
2019-08-10 10:52:48 +0800

    手动切割序列后进行滤波和转置

---
+++ Dataset Maker/manual_sequence_cutting_filtering.m
@@ -0,0 +1,11 @@
+%clear;
+load('G:\无源感知研究\数据采集\2019_07_18\实验室.mat');
+sample_num = size(csi_train,1);
+
+for i=1:sample_num
+    amplitude = csi_train{i,1}';%对序列进行转置
+    amplitude = wifi_butterworth_function(amplitude);%进行滤波
+    csi_train{i,1}=amplitude;%写回序列
+end
+
+csi_label = categorical(csi_label(:,1));
