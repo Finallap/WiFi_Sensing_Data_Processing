0b3292056b3289fa7bfbb7f099a126ea85ef26f3
Finallap <547805712@qq.com>
2019-11-07 21:03:22 +0800

    1、增加resample选项

--- LSTM/job_dispatch.m
+++ LSTM/job_dispatch.m
@@ -4,17 +4,18 @@ jobs =cell(job_num,1);
 
 %-----------------------从这里开始改各种配置------------------------%
 job_options.dataset_dir = 'G:\无源感知研究\数据采集\2019_07_18\';
-job_options.dataset_filename = '会议室(3t3r)(1)';
-job_options.result_dir = 'G:\无源感知研究\实验结果\';
+job_options.dataset_filename = '实验室(3t3r)(1)';
+job_options.result_dir = 'G:\无源感知研究\实验结果\2019_11_07_resample\';
 
 job_options.dataset_path = sprintf('%s%s%s',job_options.dataset_dir,job_options.dataset_filename,'.mat');
 job_options.hampel_flag = true;
 job_options.butterworth_flag = true;
+job_options.resample_flag = true;
 job_options.normalize_flag =true;
 
 job_options.networkType = 'SingleBiLSTM';
 job_options.inputSize = 270;
-job_options.numHiddenUnits = 200;
+job_options.numHiddenUnits = 150;
 job_options.numClasses = 6;
 job_options.maxEpochs = 30;
 job_options.isLrReduce = true;
@@ -25,17 +26,18 @@ jobs(1,1) = {job_options};
 
 %-----------------------从这里开始改各种配置------------------------%
 job_options.dataset_dir = 'G:\无源感知研究\数据采集\2019_07_18\';
-job_options.dataset_filename = '实验室(3t3r)(1)';
-job_options.result_dir = 'G:\无源感知研究\实验结果\';
+job_options.dataset_filename = '会议室(3t3r)(1)';
+job_options.result_dir = 'G:\无源感知研究\实验结果\2019_11_07_resample\';
 
 job_options.dataset_path = sprintf('%s%s%s',job_options.dataset_dir,job_options.dataset_filename,'.mat');
 job_options.hampel_flag = true;
 job_options.butterworth_flag = true;
+job_options.resample_flag = true;
 job_options.normalize_flag =true;
 
 job_options.networkType = 'SingleBiLSTM';
 job_options.inputSize = 270;
-job_options.numHiddenUnits = 200;
+job_options.numHiddenUnits = 150;
 job_options.numClasses = 6;
 job_options.maxEpochs = 30;
 job_options.isLrReduce = true;
@@ -45,10 +47,10 @@ jobs(2,1) = {job_options};
 %-----------------------到这里配置结束，并塞入jobs------------------------%
 
 % 初始化结果（使用cell结构，与输出同尺寸，每个元素对应Excel一个格子）
-result_data = cell(job_num+1,28);
+result_data = cell(job_num+1,29);
 % 建立表头
 title = {'训练开始时间','训练结束时间','数据集','输入数据维数','模型类型','隐层数量'...
-    '迭代次数','是否学习率下降','是否hampel滤波','是否低通滤波','是否归一化'...
+    '迭代次数','是否学习率下降','是否hampel滤波','是否低通滤波','是否重采样','是否归一化'...
     '1','2','3','4','5','6','7','8','9','10','平均Accuracy','平均Precision'...
     '平均Recall','平均F_score','预处理平均耗时','训练平均耗时','评估平均耗时'};
 result_data(1,:)=title;
@@ -76,15 +78,16 @@ for i = 1:job_num
     result_data(i+1,8)=num2cell(jobs{i,1}.isLrReduce);%是否学习率下降
     result_data(i+1,9)=num2cell(jobs{i,1}.hampel_flag);%是否hampel滤波
     result_data(i+1,10)=num2cell(jobs{i,1}.butterworth_flag);%是否低通滤波
-    result_data(i+1,11)=num2cell(jobs{i,1}.normalize_flag);%是否归一化
-    result_data(i+1,12:21)=num2cell(acc_count());%准确度
-    result_data(i+1,22)=num2cell(mean(acc_count));%平均准确度
-    result_data(i+1,23)=num2cell(mean(precision_count));%平均precision
-    result_data(i+1,24)=num2cell(mean(recall_count));%平均recall
-    result_data(i+1,25)=num2cell(mean(f1_count));%平均f1
-    result_data(i+1,26)=num2cell(t.preprocessed + mean(t_count.preprocessed));%预处理平均耗时
-    result_data(i+1,27)=num2cell(mean(t_count.training));%训练平均耗时
-    result_data(i+1,28)=num2cell(mean(t_count.prediction));%评估平均耗时
+    result_data(i+1,11)=num2cell(jobs{i,1}.butterworth_flag);%是否重采样
+    result_data(i+1,12)=num2cell(jobs{i,1}.normalize_flag);%是否归一化
+    result_data(i+1,13:22)=num2cell(acc_count());%准确度
+    result_data(i+1,23)=num2cell(mean(acc_count));%平均准确度
+    result_data(i+1,24)=num2cell(mean(precision_count));%平均precision
+    result_data(i+1,25)=num2cell(mean(recall_count));%平均recall
+    result_data(i+1,26)=num2cell(mean(f1_count));%平均f1
+    result_data(i+1,27)=num2cell(t.preprocessed + mean(t_count.preprocessed));%预处理平均耗时
+    result_data(i+1,28)=num2cell(mean(t_count.training));%训练平均耗时
+    result_data(i+1,29)=num2cell(mean(t_count.prediction));%评估平均耗时
 end
 
 nowtime = fix(clock);
--- LSTM/tools/dataset_reading_and_preprocessing.m
+++ LSTM/tools/dataset_reading_and_preprocessing.m
@@ -2,7 +2,13 @@
 function [csi_train,csi_label] = dataset_reading_and_preprocessing(job_options)
     load(job_options.dataset_path);
     sample_num = size(csi_train,1);
-
+    
+    %求出各个序列的长度
+    sequenceLengths = zeros(sample_num,1);
+    for i=1:sample_num
+        sequenceLengths(i) = size(csi_train{i},1);
+    end
+    
     for i=1:sample_num
         amplitude = csi_train{i,1}';%对序列进行转置
         if(job_options.hampel_flag==true)
@@ -10,6 +16,10 @@ function [csi_train,csi_label] = dataset_reading_and_preprocessing(job_options)
         end
         if(job_options.butterworth_flag==true)
             amplitude = wifi_butterworth_function(amplitude);%进行滤波
+        end
+        if(job_options.resample_flag==true)
+            amplitude = resample(amplitude',ceil(mean(sequenceLengths)),size(amplitude,2));
+            amplitude = amplitude';
         end
         if(job_options.normalize_flag==true)
             amplitude = normalize(amplitude);%归一化处理
