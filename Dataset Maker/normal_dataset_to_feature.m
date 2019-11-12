d30ac6585af2b4c3fa9e653d1bcdfe3fd19ba417
Finallap <547805712@qq.com>
2019-11-12 18:02:13 +0800

    1、增加了使用特征模型进行评估的代码

--- Dataset Maker/normal_dataset_to_feature.m
+++ Dataset Maker/normal_dataset_to_feature.m
@@ -1,31 +1,34 @@
 %clear;
 %load('matlab.mat');
+clear train_data_array train_label_array
 
-if(exist('data_array','var')==0)
-    data_array = [];
-    data_array1 = [];
-end
+train_data = x_train;%x_train x_test
+train_label = y_train;%y_train y_test
+input_shape = 270;
 
-numObservations = numel(csi_train);
+numObservations = numel(train_data);
+train_data_array = zeros(numObservations,input_shape+1);
+train_label_array = zeros(numObservations,1);
 
 for n = 1:numObservations
-    amplitude = csi_train{n};
+    amplitude = train_data{n};
     m = 1;
-    for k=1:270
-        data_array (n,m) =  mean(amplitude(k,:));%均值
+    for k=1:input_shape
+        train_data_array (n,m) =  mean(amplitude(k,:));%均值
         m = m+1;
-        %data_array (n,m) =  std(amplitude(k,:));%标准差
+        %train_data_array (n,m) =  std(amplitude(k,:));%标准差
         %m = m+1;
-        %data_array (n,m) =  var(amplitude(k,:));%方差
+        %train_data_array (n,m) =  var(amplitude(k,:));%方差
         %m = m+1;
-        %data_array (n,m) =  std(amplitude(k,:))/mean(amplitude(k,:));%变异系数c.v.
+        %train_data_array (n,m) =  std(amplitude(k,:))/mean(amplitude(k,:));%变异系数c.v.
         %m = m+1;
     end
     %data_array (n,2*3*30*3+1) =  csi_label(n);
-    data_array1 (n,1) =  csi_label(n);
+    train_label_array (n,1) =  train_label(n);
 end
 
-data_array(:,end+1)=data_array1;
-%yfit = trainedModel.predictFcn(data_array(:,1:540));
-%cm = confusionchart(data_array1,yfit);
-%[Accuracy,Precision,Recall,F_score]=indicator_calculation(yfit,data_array1)
+train_data_array(:,end)=train_label_array;
+clear k m n numObservations amplitude
+%yfit = trainedModel.predictFcn(train_data_array(:,1:270));
+%cm = confusionchart(train_label_array,yfit);
+%[Accuracy,Precision,Recall,F_score]=indicator_calculation(train_label_array,yfit)
