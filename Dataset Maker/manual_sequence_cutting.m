80165e61918ee0490fdfb2f356962d6c7c4fefe0
Finallap <547805712@qq.com>
2019-11-16 09:00:07 +0800

    1、增加RSSI的读取选择

--- Dataset Maker/manual_sequence_cutting.m
+++ Dataset Maker/manual_sequence_cutting.m
@@ -1,6 +1,7 @@
 %先载入数据，制作sequence_label标签数组
+rssi = rssi_reading('G:\无源感知研究\数据采集\2019_07_18\实验室\clap_chen_2.dat')';
 %amplitude = csi_amplitude_reading('G:\无源感知研究\数据采集\2019_07_18\实验室\clap_chen_2.dat')';
-phase = csi_phase_reading('G:\无源感知研究\数据采集\2019_07_18\实验室\pull_chen_2.dat')';
+%phase = csi_phase_reading('G:\无源感知研究\数据采集\2019_07_18\实验室\pull_chen_2.dat')';
 
 if(exist('csi_train','var')==0)
     csi_train = {};
@@ -13,15 +14,17 @@ person_label_name = 'chen';
 for i=1:length(sequence_label)
     start = sequence_label(i,1);
     the_end = sequence_label(i,2);
+    selection_rssi_sequence = rssi(start:the_end,:);%选取幅值
     %selection_amplitude_sequence = amplitude(start:the_end,:);%选取幅值
-    selection_phase_sequence = phase(start:the_end,:);%选取相位
-    selection_phase_sequence = unwrap(selection_phase_sequence);%相位解缠
+    %selection_phase_sequence = phase(start:the_end,:);%选取相位
+    %selection_phase_sequence = unwrap(selection_phase_sequence);%相位解缠
     
     csi_label_size = size(csi_label);
     csi_label_length = csi_label_size(1);
     
+    csi_train{end+1,1} = selection_rssi_sequence;%添加rssi信息
     %csi_train{end+1,1} = selection_amplitude_sequence;%添加幅值信息
-    csi_train{end+1,1} = selection_phase_sequence;%添加相位信息
+    %csi_train{end+1,1} = selection_phase_sequence;%添加相位信息
     csi_label{csi_label_length + 1,1} = label_name;
     csi_label{csi_label_length + 1,2} = person_label_name;
 end
--- Dataset Maker/manual_sequence_cutting_2.m
+++ Dataset Maker/manual_sequence_cutting_2.m
@@ -1,7 +1,7 @@
 %先载入数据，制作sequence_label标签数组
 csi_train = cell(length(sequence_label),1);
 csi_label = cell(length(sequence_label),2);
-fir_path = 'G:\无源感知研究\数据采集\2019_07_18\实验室\';
+fir_path = 'G:\无源感知研究\数据采集\2019_07_18\会议室\';
 
 %sequence_label第一列为起点，第二列为终点
 for i=1:length(sequence_label)
@@ -13,16 +13,18 @@ for i=1:length(sequence_label)
     disp(datapath)
     %amplitude = csi_amplitude_reading_and_interpolation(datapath)';
     %phase = csi_phase_reading(datapath)';
+    rssi = rssi_reading(datapath)';
     
     start = sequence_label{i,4};
     the_end = sequence_label{i,5};
-    amplitude = csi_amplitude_reading_and_interpolation(datapath,start,the_end)';
+    %amplitude = csi_amplitude_reading_and_interpolation(datapath,start,the_end)';
+    selection_rssi_sequence = rssi(start:the_end,:);%选取rssi
     %selection_amplitude_sequence = amplitude(start:the_end,:);%选取幅值
     %selection_phase_sequence = phase(start:the_end,:);%选取相位
     %selection_phase_sequence = unwrap(selection_phase_sequence);%相位解缠
     %selection_sequence = [selection_amplitude_sequence selection_phase_sequence];
     
-    csi_train{i,1} = amplitude;
+    csi_train{i,1} = selection_rssi_sequence;
     csi_label{i,1} = label_name;
     csi_label{i,2} = person_label_name;
 end
