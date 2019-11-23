6ae447c9de433b779dbfc50cf79556a32a70fc53
Finallap <547805712@qq.com>
2019-11-23 11:11:08 +0800

    1、相位稳定的代码

---
+++ Dataset Maker/Tools/sanitize_phase.m
@@ -0,0 +1,9 @@
+% Phase Transformation
+function output_phase=sanitize_phase(input_phase)
+
+x = unwrap(input_phase);
+a = ( x(30)-x(1) )/ 30;
+b = sum(x)/30;
+% range = [-15:-1 , 1:15];
+range=[-28:2:-1,-1,1:2:28,28];
+output_phase = x - (a*range + b*ones(1,30));
