d72ddbe9c7e91d713d84990627c4dc0cafda35d1
Finallap <547805712@qq.com>
2019-11-28 16:44:42 +0800

    一个错误的预处理过程（主要是维度写反了，但是还是备份一下）

---
+++ Dataset Maker/manual_sequence_cutting_filtering_2.m
@@ -0,0 +1,21 @@
+clear;
+load('G:\无源感知研究\数据采集\2019_07_18\实验室(3t3r).mat');
+sample_num = size(csi_train,1);
+
+for i=1:sample_num
+    amplitude = csi_train{i};
+    sequenceLengths(i) = size(amplitude,1);
+end
+
+for i=1:sample_num
+    amplitude = csi_train{i,1};%读取序列
+    amplitude = hampel(amplitude');%hampel滤波
+    amplitude = wifi_butterworth_function(amplitude);%butterworth滤波
+    amplitude = resample(amplitude',ceil(mean(sequenceLengths)),size(amplitude,2));
+    %[amplitude,PS]=mapminmax(amplitude);
+    amplitude = normalize(amplitude);%归一化处理
+    csi_train{i,1}=amplitude';%写回序列
+end
+
+csi_label = categorical(csi_label(:,1));
+clear sample_num i amplitude
