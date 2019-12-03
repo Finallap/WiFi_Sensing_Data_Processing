8f0c93b96bc8e50cbc6573179be75f860b27a5b9
Finallap <547805712@qq.com>
2019-12-03 11:28:23 +0800

    网传的保存图片论文代码，貌似并不好使

--- Data Preprocessing/filtering_comparison_chart_drawing.m
+++ Data Preprocessing/filtering_comparison_chart_drawing.m
@@ -11,6 +11,7 @@ set(gca,'XLim',[0 time_length])
 legend('original signal')
 xlabel('Time(Seconds)')
 ylabel('CSI amplitude')
+%save_figure(figure,'original.')
 
 %figure('color',[1 1 1])
 %hampel(amplitude)
@@ -28,6 +29,7 @@ set(gca,'XLim',[0 time_length])
 legend('abnormal original signal','hampel filtered signal','outliers')
 xlabel('Time(Seconds)')
 ylabel('CSI amplitude')
+%save_figure(figure,'hampel.')
 
 figure('color',[1 1 1])
 plot(t,wifi_butterworth_function(amplitude_hampel'),'k')
@@ -35,3 +37,14 @@ set(gca,'XLim',[0 time_length])
 legend('butterworth filtered signal')
 xlabel('Time(Seconds)')
 ylabel('CSI amplitude')
+%save_figure(figure,'butterworth.')
+
+function save_figure(figure,savename)
+    hfig = figure;
+    figWidth = 5;  % 设置图片宽度
+    figHeight = 5;  % 设置图片高度
+    set(hfig,'PaperUnits','inches'); % 图片尺寸所用单位
+    set(hfig,'PaperPosition',[0 0 figWidth figHeight]);
+    fileout = [savename]; % 输出图片的文件名
+    print(hfig,[fileout,'tif'],'-r600','-dtiff'); % 设置图片格式、分辨率
+end
